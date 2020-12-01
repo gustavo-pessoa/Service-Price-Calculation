4f1450a416812c96c20c206f1fdfa95d029141d4
Gustavo Pessoa <gustavo.pessoa@gmail.com>
2020-12-01 21:51:27 +0000

    202012012151

---
+++ scripts/script.js
@@ -0,0 +1,22 @@
+let inputCotacaoDoDolar = document.getElementById("cotacaoDoDolar"); 
+let inputTempoDeContrato = document.getElementById("tempoDeContrato"); 
+let inputDistancia = document.getElementById("distancia"); 
+let inputTempoDeDeslocamento = document.getElementById("tempoDeDeslocamento"); 
+let inputValorDePedagio = document.getElementById("valorDePedagio"); 
+let inputServidores = document.getElementById("servidores"); 
+let inputComputadores = document.getElementById("computadores"); 
+let inputImpressoras = document.getElementById("impressoras"); 
+let inputSwitch = document.getElementById("switch"); 
+let inputMonitoramento = document.getElementById("monitoramento "); 
+let inputAntiVirus = document.getElementById("antiVirus"); 
+let inputWebProtection = document.getElementById("webProtection"); 
+let inputRiskIntelligence = document.getElementById("riskIntelligence"); 
+let inputBackup = document.getElementById("backup"); 
+let inputpsi = document.getElementById("psi"); 
+let inputpcn = document.getElementById("pcn"); 
+
+let resultado = document.getElementById("resultado");
+
+function calcular(){
+    console.log("Calculando...")
+}
